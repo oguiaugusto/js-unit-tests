2939e28ca9b6fb926b7097b985f8fac76bd5cae7
gui <guilhaugusto05@gmail.com>
2021-09-29 15:17:47 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,15 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  if (array.length === 0) return undefined;
+  let soma = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') return undefined;
+    soma += array[i];
+  }
+  const media = soma / array.length;
+  return Math.round(media);
 };
 
 module.exports = average;
